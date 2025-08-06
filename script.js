c9bd5d3f8448df2ed47019ba1a0e77e96449de7d
jatavsf429098-pixel <jatavsf429098@gmail.com>
2025-08-06 15:04:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,26 @@ document.addEventListener("DOMContentLoaded", () => {
           tool.name.toLowerCase().includes(value) ||
           tool.description.toLowerCase().includes(value) ||
           tool.category.toLowerCase().includes(value)
-        );
+        );// Theme Toggle
+const toggleBtn = document.getElementById("theme-toggle");
+const userTheme = localStorage.getItem("theme");
+
+if (userTheme === "dark") {
+  document.body.classList.add("dark");
+  toggleBtn.textContent = "☀️ Light Mode";
+}
+
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+    toggleBtn.textContent = "☀️ Light Mode";
+  } else {
+    localStorage.setItem("theme", "light");
+    toggleBtn.textContent = "🌙 Dark Mode";
+  }
+});
+
         displayTools(filtered);
       });
 
